b792f7fe1ead91b99be1235e4e40ffbc8804a548
bragcat <bragcat.li@gmail.com>
2019-01-28 12:06:40 +0800

    810, construction

---
+++ 810.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool xorGame(vector<int>& nums) {
+        int x = 0;
+        for (int num : nums) {
+            x = x ^ num;
+        }
+        return x == 0 || nums.size() % 2 == 0;
+    }
+};
