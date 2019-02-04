1e61d5645203d4bc7cec329c39e9ae51e2a2501c
bragcat <bragcat.li@gmail.com>
2019-02-04 20:56:42 +0800

    601, mysql

---
+++ 601.cpp
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT s1.* FROM stadium AS s1, stadium AS s2, stadium AS s3 
+    WHERE ((s1.id + 1 = s2.id AND s1.id + 2 = s3.id) OR (s1.id - 1 = s2.id AND s1.id + 1 = s3.id) OR (s1.id - 2 = s2.id AND s1.id - 1 = s3.id)) AND s1.people >= 100 AND s2.people >= 100 AND s3.people >= 100 
+    GROUP BY s1.id;
