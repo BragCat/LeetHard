31de32524534c2544b570b2dc6623bf616e4b1d5
bragcat <bragcat.li@gmail.com>
2019-09-22 10:24:20 +0800

    410, binary search answer, dynamic programming

--- 410.cpp
+++ 410.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     int splitArray(vector<int>& nums, int m) {
-        vector<int> sum{nums[0]};
-        int n = nums.size();
-        for (int i = 1; i < n; ++i) {
-            sum.push_back(sum.back() + nums[i]);
-        }
-        vector<vector<int>> f(n, vector<int>(m, MAX_INT));
-        f[0][0] = sum[0];
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j <= min(i, m - 1); ++j) {
-                if (j == 0) {
-                    f[i][j] = sum[i];
-                } else {
-                    for (int k = 0; k < i; ++k) {
-                        f[i][j] = min(f[i][j], max(f[k][j - 1], sum[i] - sum[k]));
-                    }
-                }
+        LL l = *max_element(nums.begin(), nums.end()), r = accumulate(nums.begin(), nums.end(), 0L);
+        LL ans = -1;
+        while (l <= r) {
+            LL mid = (l + r) / 2;
+            if (check(nums, mid, m)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
-        return f[n - 1][m - 1];
+        return ans;
     }
 private:
-    const int MAX_INT = ~0u >> 1;
+    using LL = long long;
+    bool check(vector<int>& nums, LL bound, int m) {
+        int cnt = 1;
+        LL sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum = sum + nums[i];
+            if (sum > bound) {
+                ++cnt;
+                sum = nums[i];
+            }
+        }
+        return cnt <= m;
+    }
 };
